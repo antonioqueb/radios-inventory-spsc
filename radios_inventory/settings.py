c977f48e1904e9f61e42c575f70c40d1b49628f3
querubcastillo <cardstillo@gmail.com>
2023-02-28 15:51:01 -0600

    create folder API

--- radios_inventory/settings.py
+++ radios_inventory/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'servicios.apps.ServiciosConfig',
 ]
 
@@ -49,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware"
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost'
 ]
 
 ROOT_URLCONF = 'radios_inventory.urls'
